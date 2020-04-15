b8d893c89ff5f8c323bfe7c0c4eabd8d109db7a7
Mohammad Kabir <mkabir@semanticsoftware.com.au>
2020-04-15 21:39:59 +1000

    d3 installed and first circle visible

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import * as d3 from "d3";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {};
+  componentDidMount() {
+    const svg = d3.select("svg");
+    const height = svg.attr("height");
+    const width = svg.attr("width");
+
+    svg
+      .append("circle")
+      .attr("r", height / 2 - 50)
+      .attr("cx", width / 2)
+      .attr("cy", height / 2);
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <svg style={{ background: "lightblue" }} width="800" height="500"></svg>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
